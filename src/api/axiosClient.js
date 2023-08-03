b897de21fc978d26a5d132173e2acd69ab34afe7
unknown <eiad.omar88@gmail.com>
2023-08-04 00:21:54 +0300

    API Configuration

---
+++ src/api/axiosClient.js
@@ -0,0 +1,29 @@
+import api from './apiConfig';
+import axios from 'axios';
+import queryString from 'query-string';
+
+const axiosClient = axios.create({
+  baseUrl: api.baseURL,
+  headers: {
+    'Content-tpye': 'application/json',
+  },
+  paramsSerializer: (params) =>
+    queryString.stringify({ ...params, api_key: api.apiKey }),
+});
+
+axiosClient.interceptors.request.use(async (config) => config);
+
+axiosClient.interceptors.response.use(
+  (response) => {
+    if (response && response.data) {
+      return response.data;
+    }
+
+    return response;
+  },
+  (error) => {
+    throw error;
+  }
+);
+
+export default axiosClient;
