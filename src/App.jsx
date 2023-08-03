9bf88c1b16c6bb1deef231dd4d2ab7e1ebe63803
EyadOmar <eiad.omar88@gmail.com>
2023-08-04 01:42:27 +0300

    configured app routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
+import { Route, Routes } from 'react-router-dom';
+import Header from './components/header/Header';
+import Footer from './components/footer/Footer';
+import Home from './pages/Home';
+import Details from './pages/Details';
+import Catalog from './pages/Catalog';
 function App() {
   return (
     <>
-      <a href="/#">wooow</a>
+      <Header />
+      <Routes>
+        <Route path="/" Component={Home} />
+        <Route path="/:category/:id" Component={Details} />
+        <Route path="/:category/search/:searchTerm" Component={Catalog} />
+        <Route path="/:category" Component={Catalog} />
+      </Routes>
+      <Footer />
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.scss';
+import 'swiper/css';
+import './assets/boxicons-2.0.7/css/boxicons.min.css';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>
+  </BrowserRouter>
 );
