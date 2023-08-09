716c463afdcdca5e448e5e1766213b429ca5a000
EyadOmar <eiad.omar88@gmail.com>
2023-08-09 20:29:31 +0300

    fixed view more button

--- src/components/movie-list/MovieSection.jsx
+++ src/components/movie-list/MovieSection.jsx
@@ -1,13 +1,15 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { OutLinedButton } from '../button/Button';
 import MoviesList from './MoviesList';
 
 function MovieSecion({ title, cate, type }) {
+  const nav = useNavigate();
+
   return (
     <section className="mb-3 container">
       <div className="section_header mb-2">
         <h2>{title}</h2>
-        <Link to={"'/movie"}>
+        <Link to={`/${cate}`}>
           <OutLinedButton classNameProp="small">View More</OutLinedButton>
         </Link>
       </div>
